7bd3323730b18af36a675012be46d562e752f6da
TanPicK <taniapichierri88@gmail.com>
2023-08-10 11:57:37 +0200

    Imported existing project

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer class="col-md-12 footer">
+      <a
+        href="https://github.com/TanPicK/weather-app-react2"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Git Hub poject page"
+      >
+        {" "}
+        Open-source code{" "}
+      </a>{" "}
+      by Tania Pichierri
+    </footer>
+  );
+}
